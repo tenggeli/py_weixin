a9fd90d8c9c223c59237c7f567a4a2f87361cd8a
echo <tenggeliguge@gmail.com>
2017-11-05 12:15:42 +0800

    test

--- main.py
+++ main.py
@@ -3,17 +3,12 @@
 import web
 from handle import Handle
 
+
 urls = (
     '/wx', 'Handle',
 )
 
-app = web.application(urls, globals())
-#返回wsgi接口
-# 需要在main外面，uwsgi调用不会执行main
-application = app.wsgifunc()
-
 if __name__ == '__main__':
-    # app = web.application(urls, globals())
-    # application = app.wsgifunc()
+    app = web.application(urls, globals())
     app.run()
 
