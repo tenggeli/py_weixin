053bd56299fce582dd252cc180b4cb32aa50997e
echo <tenggeliguge@gmail.com>
2017-11-05 10:35:07 +0800

    change run

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ urls = (
     '/wx', 'Handle',
 )
 
+app = web.application(urls, globals())
+#返回wsgi接口
+# 需要在main外面，uwsgi调用不会执行main
+application = app.wsgifunc()
+
 if __name__ == '__main__':
-    app = web.application(urls, globals())
+    # app = web.application(urls, globals())
+    # application = app.wsgifunc()
     app.run()
