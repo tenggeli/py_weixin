c221abe0102bda75006e6f02922f42330bcbefc0
echo <tenggeliguge@gmail.com>
2017-11-15 21:46:48 +0800

    xiugai

--- handle.py
+++ handle.py
@@ -5,11 +5,43 @@ import reply
 import receive
 import web
 
+import logging
+logging.basicConfig(filename='/mnt/log/nginx/python.log',level=logging.DEBUG)
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+
 class Handle(object):
+    def GET(self):
+        try:
+            data = web.input()
+            if len(data) == 0:
+                return "hello, this is handle view"
+            signature = data.signature
+            timestamp = data.timestamp
+            nonce = data.nonce
+            echostr = data.echostr
+            token = "mianduixianshi19921223"  # 请按照公众平台官网\基本配置中信息填写
+
+            list = [token, timestamp, nonce]
+            list.sort()
+            sha1 = hashlib.sha1()
+            map(sha1.update, list)
+            hashcode = sha1.hexdigest()
+            print "handle/GET func: hashcode, signature: ", hashcode, signature
+            if hashcode == signature:
+                return echostr
+            else:
+                return ""
+        except Exception, Argument:
+            return Argument
+'''
+
     def POST(self):
         try:
             webData = web.data()
             print "Handle Post webdata is ", webData   #后台打日志
+            logging.warning('Handle Post webdata is  {}'.format('webData'))
             recMsg = receive.parse_xml(webData)
             if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
                 toUser = recMsg.FromUserName
@@ -22,3 +54,4 @@ class Handle(object):
                 return "success"
         except Exception, Argment:
             return Argment
+'''
